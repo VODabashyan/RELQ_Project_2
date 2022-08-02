d98aa6a6d2e5f295f7ca9942f596e963b5c4c478
VODabashyan <vaheodabashyan@gmail.com>
2022-08-02 15:13:18 +0400

    added functions to fetch and show data from API.

---
+++ script.js
@@ -0,0 +1,16 @@
+let weather = {
+    apiKey: "1e701c14e277dc8f2c4fa1f7ae89aa3e",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, 
+            speed);
+    }
+}
